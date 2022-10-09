14abaea12ea294abd1eea0350d6911cdf910c90f
jewoo <jeus0630@gmail.com>
2022-10-09 16:30:57 +0900

    Add security

---
+++ src/main/java/com/example/security1/model/User.java
@@ -0,0 +1,31 @@
+package com.example.security1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
